c84b1560c70c80bf732cb76e6e0efed39e76f33a
elkatrachuk <elkatrachuk@gmail.com>
2022-06-05 16:34:38 +0200

    create login endpoint

--- routers/auth.js
+++ routers/auth.js
@@ -18,9 +18,34 @@ router.post("/signup", async (req, res, next) => {
     });
     delete newUser.dataValues.password;
     const token = jwt.sign({ userId: newUser.id }, SECRET, { expiresIn: "2h" });
-    res.send({ newUser, token });
+    res.send({ user: newUser, token });
   } catch (e) {
     next(e);
+  }
+});
+
+router.post("/login", async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    if (!email || !password) {
+      return res
+        .status(400)
+        .send({ message: "Please provide both email and password" });
+    }
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user || !bcrypt.compareSync(password, user.password)) {
+      return res.status(400).send({
+        message: "User with that email not found or password incorrect",
+      });
+    }
+    delete user.dataValues["password"]; // don't send back the password hash
+    const token = jwt.sign({ userId: user.id }, SECRET, { expiresIn: "2h" });
+    return res.status(200).send({ token, user: user.dataValues });
+  } catch (error) {
+    return res.status(400).send({ message: "Something went wrong, sorry" });
   }
 });
 
